f8802783ee56dca615fadef29e85db475341b2c6
baz2222 <baz2222@i.ua>
2018-06-01 16:23:14 +0200

    01-06-2018 cant launch activity with libgdx from splash screen

---
+++ src/com/juniorgames/spacestrikerandroid2/GameScreenActivity.java
@@ -0,0 +1,16 @@
+package com.juniorgames.spacestrikerandroid2;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+import android.os.Bundle;
+
+public class GameScreenActivity extends AndroidApplication {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        initialize(new SpaceStriker(),config);
+    }
+}
--- src/com/juniorgames/spacestrikerandroid2/SplashScreenActivity.java
+++ src/com/juniorgames/spacestrikerandroid2/SplashScreenActivity.java
@@ -1,16 +1,25 @@
 package com.juniorgames.spacestrikerandroid2;
 
-import com.badlogic.gdx.backends.android.AndroidApplication;
-import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.juniorgames.spacestrikerandroid.R;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
-public class SplashScreenActivity extends AndroidApplication {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-        initialize(new SpaceStriker(),config);
-    }
+public class SplashScreenActivity extends Activity {
+private static int SST = 5000;
+@Override 
+protected void onCreate(Bundle s){
+	super.onCreate(s);
+	setContentView(R.layout.activity_splash_screen);
+	new Handler().postDelayed(new Runnable(){
+		@Override
+		public void run(){
+			Intent i=new Intent(SplashScreenActivity.this,GameScreenActivity.class);
+			startActivity(i);
+			finish();//close this activity
+		}
+	},SST);
+}
 }
