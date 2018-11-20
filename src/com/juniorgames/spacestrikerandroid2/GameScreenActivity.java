a14c7f295c47b5aa82313fff2782696680f5dad9
Vasiliy Velgus <baz2222@i.ua>
2018-11-20 13:54:25 +0200

    libgdx works with native android activitiesgit add .git add .git add .git add .!

--- src/com/juniorgames/spacestrikerandroid2/GameScreenActivity.java
+++ src/com/juniorgames/spacestrikerandroid2/GameScreenActivity.java
@@ -4,12 +4,10 @@ import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
 import android.os.Bundle;
-
 public class GameScreenActivity extends AndroidApplication {
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle s) {
+        super.onCreate(s);
         AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
         initialize(new SpaceStriker(),config);
     }
